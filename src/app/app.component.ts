659b5c3a46e345aab3db60350e2b37ffc7a421fa
Dishant <deeshant29@gmail.com>
2025-01-26 19:38:57 +0530

    added header, night mode & translate module in project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,35 @@ import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { environment } from '../../environments/environment';
+import { HeaderComponent } from "./header/header.component";
+import { NgxTranslateModule } from './translate/translate.module';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, NgxTranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'todo-list';
   readonly environment = environment;
+  siteLanguage = 'English';
+  languageList = [
+    { code: 'en', label: 'English' },
+    { code: 'mr', label: 'Marathi' },
+  ];
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  constructor(@Inject(PLATFORM_ID) private platformId: Object,
+  private translate: TranslateService) {
+  }
 
   ngAfterViewInit() {
+    this.toggleMode();
+  }
+
+  toggleMode(): void {
     if (isPlatformBrowser(this.platformId)) {
       // Perform DOM manipulation only if running in the browser
       if (environment?.isDark) {
@@ -28,4 +42,17 @@ export class AppComponent {
       }
     }
   }
+
+  changeSiteLanguage(localeCode: string): void {
+    const selectedLanguage = this.languageList
+      .find((language) => language.code === localeCode)
+      ?.label.toString();
+    if (selectedLanguage) {
+      this.siteLanguage = selectedLanguage;
+      this.translate.use(localeCode);
+    }
+    const currentLanguage = this.translate.currentLang;
+    console.log('currentLanguage', currentLanguage);
+  }
+  
 }
